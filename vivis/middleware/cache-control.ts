d78e13fe2d6c8fe35ffb407d92afefa0162c61c0
Tim <tim_menzel@t-online.de>
2023-07-13 11:07:56 +0200

    adjusted cache max-age

--- vivis/middleware/cache-control.ts
+++ vivis/middleware/cache-control.ts
@@ -1,10 +1,10 @@
 export default defineEventHandler((event) => {
 	const res = event.node.res;
-	const year = 60 * 60 * 24 * 4;
-	const hour = 60 * 60;
+	const year = 60 * 60 * 24 * 4 * 12;
+	const hour = 60 * 60 * 24;
 	const url = event.node.req.url;
 	if (url) {
-		const maxage = url.match(/(.+)\.(jpg|jpeg|gif|css|png|js|ico|svg|mjs)/) ? year : hour;
+		const maxage = url.match(/(.+)\.(jpg|jpeg|gif|css|png|js|ico|svg|mjs|webp)/) ? year : hour;
 		res.setHeader("Cache-Control", `max-age=${maxage} s-maxage=${maxage}`);
 	}
 });
